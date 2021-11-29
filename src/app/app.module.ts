2b7778600173d716fc0b35b4a6ec61f5ff80467b
dianasfigueroa <diana.figueroa@newhorizons.edu.do>
2021-11-29 18:54:47 +0000

    project dominican republic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { ButtonComponent } from './button/button.component';
+import { Button2Component } from './button2/button2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ButtonComponent,
+    Button2Component
   ],
   imports: [
     BrowserModule,
